f145695bddabaa5bbdc68914f47f23190216f0bd
Roasted-Ice <z1470603076@163.com>
2022-12-13 15:00:29 +0800

    完成二维码生成网址

--- front-end/src/pages/testHome/testHome.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<!--    <HomePage slot=""></HomePage>-->
-    <HomePage></HomePage>
-</template>
-<script setup lang="ts">
-import HomePage from "../HomePage/HomePage.vue";
-</script>
---
+++ front-end/src/pages/testHome/testQRCode.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>二维码生成</div>
+    <qrcode-vue :value="data.payUrl" :size="data.size" level="H" />
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import QrcodeVue from 'qrcode.vue'
+
+const data = reactive<{
+    payUrl: string
+    size: number
+}>({
+    payUrl: '千万别搭理s---b',
+    size: 256,
+})
+</script>
