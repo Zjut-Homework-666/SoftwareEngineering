df08dfeae4ad21dce1fe78d4b77df82060964373
Roasted-Ice <z1470603076@163.com>
2022-12-15 20:53:10 +0800

    完成付款界面bug修复,还剩支付成功数据填充

--- front-end/src/pages/PaymentPage/QRCode.vue
+++ front-end/src/pages/PaymentPage/QRCode.vue
@@ -2,43 +2,42 @@
     <div class="bt_ct">
         <el-form class="bt_form" label-width="70px">
             <p class="title">请扫描二维码付款</p>
-           <!-- <img id="QRCode" src="../../assets/QRCode.jpg" alt="" width=300 height=300> -->
-<!--            <vue-qr text="Hello world!" :callback="test" qid="testid"></vue-qr>-->
             <vue-qr background-color="#f8f6f6" :text='QRCodeURL' :size="300"></vue-qr>
             <div class="title">剩余支付时间：{{count}}</div>
+
             <el-form-item class="bt_row" label-width="0">
                 <el-button class="bt" type="primary" @click="Cancel">
                     <span>取消</span>
                 </el-button>
             </el-form-item>
-<!--            <el-dialog v-model="centerDialogVisible" title="付款成功" width="30%" align-center>-->
-<!--                <div class="dia">-->
-<!--                    <el-form class="dia_form">-->
-<!--                    <el-form-item class="dia_row" label-width="75" label="姓名:">-->
-<!--                        XXXXXX-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item class="dia_row" label-width="75" label="性别:">-->
-<!--                        XXXXXX-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item class="dia_row" label-width="75" label="身份证号:">-->
-<!--                        XXXXXX-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item class="dia_row" label-width="75" label="电话号码:">-->
-<!--                        XXXXXX-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item class="dia_row" label-width="75" label="订单编号:">-->
-<!--                        XXXXXX-->
-<!--                    </el-form-item>-->
-<!--                    </el-form>-->
-<!--                </div>-->
-<!--                <template #footer>-->
-<!--                    <span class="dialog-footer">-->
-<!--                        <el-button type="primary" @click="centerDialogVisible = false">-->
-<!--                            Confirm-->
-<!--                        </el-button>-->
-<!--                    </span>-->
-<!--                </template>-->
-<!--            </el-dialog>-->
+            <el-dialog v-model="centerDialogVisible" title="付款成功" width="30%" align-center>
+                <div class="dia">
+                    <el-form class="dia_form">
+                    <el-form-item class="dia_row" label-width="75" label="姓名:">
+                        XXXXXX
+                    </el-form-item>
+                    <el-form-item class="dia_row" label-width="75" label="性别:">
+                        XXXXXX
+                    </el-form-item>
+                    <el-form-item class="dia_row" label-width="75" label="身份证号:">
+                        XXXXXX
+                    </el-form-item>
+                    <el-form-item class="dia_row" label-width="75" label="电话号码:">
+                        XXXXXX
+                    </el-form-item>
+                    <el-form-item class="dia_row" label-width="75" label="订单编号:">
+                        XXXXXX
+                    </el-form-item>
+                    </el-form>
+                </div>
+                <template #footer>
+                    <span class="dialog-footer">
+                        <el-button type="primary" @click="centerDialogVisible = false">
+                            Confirm
+                        </el-button>
+                    </span>
+                </template>
+            </el-dialog>
         </el-form>
     </div>
 </template>
@@ -125,21 +124,20 @@
 }
 </style>
 
-<script lang="ts" setup>
+<script setup>
 import axios from 'axios';
 import router from '../../router/index.js'
 // import { useRoute } from "vue-router";
 import { ElMessageBox } from 'element-plus'
 
 import {onMounted, ref} from "vue";
-// import VueQr from 'vue-qr/src/packages/vue-qr.vue'
+import VueQr from 'vue-qr/src/packages/vue-qr.vue'
 
 
 let count = ref('');  //倒计时
 let seconds= 900; // 15分钟的秒数
 
 // const proxy :any = getCurrentInstance().appContext.config.globalProperties
-
 
 
 // let id = router.currentRoute.value.query.id
@@ -167,11 +165,12 @@ const Time = () => {
     }, 1000)
 }
 const countDown = () => {
-    let m = parseInt(seconds/60%60);
-     m = m < 10 ? 0 + m : m
+    let m = parseInt((seconds/60%60).toString());
+    m = m < 10 ? "0" + m : m
     let s = seconds % 60;
     s = s < 10 ? "0" + s : s
     count.value = m.toString() + '分' + s.toString() + '秒'
+    console.log(count.value)
     // count = '14:59'
 }
 
